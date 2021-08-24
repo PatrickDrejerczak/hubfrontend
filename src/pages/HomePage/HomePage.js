f0ed7bdc702f1844757f98abc8876612b6b1d8a8
namnhat1110 <namnhat1110@gmail.com>
2021-08-24 21:39:59 +0700

    move line chart to new menu

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,22 +1,10 @@
 import React from "react";
-// import BarChart from "../../components/BarChart";
-// import DonePieChart from "../../components/charts/totaldonevspending";
-// import TicketPieChart from "../../components/charts/totalrequestvsdonate";
 import SideBar from "../../components/SideBar";
-import DoughnutChartDonate from "../../components/DonutChartDonated";
-import DoughnutChartReceiver from "../../components/DoughnutChartReceiver";
-import TinaTestBarChart from "../../components/TinaTestBarChart";
 
 const HomePage = () => {
   return (
     <div>
       <SideBar />
-      {/* <BarChart />
-      <TicketPieChart />
-      <DonePieChart /> */}
-      {/* <DoughnutChartDonate />
-      <DoughnutChartReceiver />
-      <TinaTestBarChart /> */}
     </div>
   );
 };
