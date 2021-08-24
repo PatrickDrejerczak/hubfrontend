365e030fc24e9fafacc5143eddebe422a686abeb
Tina <nguyennga.hcm@gmail.com>
2021-08-24 09:55:44 +0700

    add donut chart

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,74 +1,67 @@
-import React, { useState, useEffect } from 'react';
-import { Bar } from 'react-chartjs-2';
-import ticketActions from '../redux/actions/ticket.action';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { Bar } from "react-chartjs-2";
+import ticketActions from "../redux/actions/ticket.action";
+import { useDispatch, useSelector } from "react-redux";
 const BACKEND_API = process.env.REACT_APP_BACKEND_API;
 
-
-
-
-
-
-
-
 const VerticalBar = () => {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(ticketActions.getTickets());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(ticketActions.getTickets());
+  }, [dispatch]);
 
-    const state = useSelector(state => state)
-    const tickets = state.ticketReducer.tickets
-    const loading = state.ticketReducer.loading
+  const state = useSelector((state) => state);
+  const tickets = state.ticketReducer.tickets;
+  const loading = state.ticketReducer.loading;
 
-    const data = {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [
-            {
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(220, 99, 135, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
-                ],
-                borderWidth: 1,
-            },
+  const data = {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          "rgba(220, 99, 135, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
         ],
-    };
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
 
-    const options = {
-        scales: {
-            yAxes: [
-                {
-                    ticks: {
-                        beginAtZero: true,
-                    },
-                },
-            ],
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
         },
-    };
+      ],
+    },
+  };
 
-    return (
-        <div>
-            <div className='header'>
-                <h1 className='title'>Vertical Bar Chart</h1>
-            </div>
-            <Bar data={data} options={options} />
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className="header">
+        <h1 className="title">Vertical Bar Chart</h1>
+      </div>
+      <Bar data={data} options={options} />
+    </div>
+  );
+};
 
-export default VerticalBar
+export default VerticalBar;
