9a0fbd6dcf2a5fd96e59f0d5fd4bb6f81e149e41
Patrick Drejerczak <drejerczak@aol.com>
2021-08-24 15:29:30 +0200

    todayrequested added

--- src/components/TodayRequest.js
+++ src/components/TodayRequest.js
@@ -14,31 +14,25 @@ const TodayRequest = () => {
   const state = useSelector((state) => state);
   const tickets = state.ticketReducer.tickets;
   const loading = state.ticketReducer.loading;
-  function gettotalTicketOf(name, kindOfItems) {
-    let totalDonateTickets = 0;
-    const itemsLength = tickets.items;
+
+  function getTotalTicketOf(ticketType) {
+    let totalTickets = 0;
     tickets.map((ticket) => {
-      if (ticket.ticketType === name && ticket.weeksAgo === 0) {
-        let items = ticket.items;
-        items.map((items) => {
-          if (items.itemName === kindOfItems) {
-            totalDonateTickets += items.quantity;
-          }
-        });
+      if (ticket.ticketType === ticketType) {
+        totalTickets += 1;
       }
     });
-    return totalDonateTickets;
+    return totalTickets;
   }
 
-  const totalReceive = gettotalTicketOf("receive");
+  const totalTickets = getTotalTicketOf("receive");
 
-  const TodayRequest = Math.sum(totalReceive);
-  console.log("sum", TodayRequest);
+  console.log(totalTickets);
 
   return (
     <div>
       <div className="header">
-        <h1 className="title"> {TodayRequest} </h1>
+        <h1 className="title"> {totalTickets} </h1>
       </div>
     </div>
   );
