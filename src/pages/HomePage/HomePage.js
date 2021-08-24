f207180698d1f6964abaa4d8373bb0dc0922dcbf
kimbua <nguyen.k.lien98@gmail.com>
2021-08-24 10:10:49 +0700

    add pie chart

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,10 +1,14 @@
 import React from "react";
 import BarChart from "../../components/BarChart";
+import DonePieChart from "../../components/charts/totaldonevspending";
+import TicketPieChart from "../../components/charts/totalrequestvsdonate";
 
 const HomePage = () => {
     return (
         <>
             <BarChart />
+            <TicketPieChart />
+            <DonePieChart/>
         </>
     );
 };
