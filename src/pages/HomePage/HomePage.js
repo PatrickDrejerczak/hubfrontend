e312bc19b0110f613452989790c710cc9429295e
kimbua <nguyen.k.lien98@gmail.com>
2021-08-24 11:35:01 +0700

    done 2 pie charts

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import BarChart from "../../components/BarChart";
-import DonePieChart from "../../components/PieCharts/totaldonevspending";
-import TicketPieChart from "../../components/PieCharts/totalrequestvsdonate";
+import DonePieChart from "../../components/PieCharts/TotalDoneAndSpending";
+import TicketPieChart from "../../components/PieCharts/TotalRequestAndDonate";
 
 const HomePage = () => {
   return (
