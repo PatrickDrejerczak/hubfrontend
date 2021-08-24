28c5bb176f5cf3cfc8e12745e7975592c9069737
namnhat1110 <namnhat1110@gmail.com>
2021-08-24 11:23:59 +0700

    pull donut

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,7 +2,7 @@ import React from "react";
 // import BarChart from "../../components/BarChart";
 // import DonePieChart from "../../components/charts/totaldonevspending";
 // import TicketPieChart from "../../components/charts/totalrequestvsdonate";
-import SideBar from "../../components/BarChart";
+import SideBar from "../../components/SideBar";
 
 const HomePage = () => {
   return (
