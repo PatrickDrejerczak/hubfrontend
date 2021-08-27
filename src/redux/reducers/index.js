27dffcae3e5f675d1f525d7346ae225507eb1a56
kimbua <nguyen.k.lien98@gmail.com>
2021-08-27 15:17:25 +0700

    add barchart

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import barchartReducer from "./barchart.reducer";
 import donutchartReducer from "./donutchart.reducer";
 import ticketReducer from "./ticket.reducer";
 
 export default combineReducers({
   donutchartReducer: donutchartReducer,
   ticketReducer: ticketReducer,
+  barchartReducer: barchartReducer,
 });
